14deea858091f90ff11512529ce937ceca85e470
Karla Sequen <karlasequen@gmail.com>
2022-12-01 08:43:08 -0600

    2022 day 1

--- src/Year2021/Constant.java
+++ src/Year2021/Constant.java
@@ -3,5 +3,6 @@ package Year2021;
 public class Constant {
 
     public static final String BASE_PATH_2021 = "src\\Year2021\\";
+    public static final String BASE_PATH_2022 = "src\\Year2022\\";
 
 }
