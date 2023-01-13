d193a28ad950a250a3a5ae73f086ca7cf9f425a1
Karla Sequen <karlasequen@gmail.com>
2023-01-13 10:53:44 -0600

    reorganize methods, class, and add common function to reuse

--- src/Year2022/day8/Day8.java
+++ src/Year2022/day8/Day8.java
@@ -1,60 +1,62 @@
 package Year2022.day8;
 
 import shared.Constant;
+import shared.InputData;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.List;
 
 public class Day8 {
 
+    //    private static final String FILE_NAME = Constant.BASE_PATH_2022 + "day8\\sample.txt";
+    private static final String FILE_NAME = Constant.BASE_PATH_2022 + "day8\\input.txt";
+
     public static void main(String[] args) {
-        try {
-            File    inputFile = new File(Constant.BASE_PATH_2022 + "day8\\input.txt");
-            Scanner myReader  = new Scanner(inputFile);
 
-            int countOfRows = 0;
+        int sizeTreeMap = 5;
 
-            int[][] treeMap = new int[99][99];
-
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-
-                if (data.isEmpty()) {
-                    continue;
-                }
-
-                treeMap[countOfRows] = Arrays.stream(data.split(""))
-                        .mapToInt(Integer::parseInt)
-                        .toArray();
-                countOfRows += 1;
-
-            }
-            myReader.close();
-
-            int countOfVisibleInteriorTrees = 0;
-            for (int row = 1; row < treeMap.length - 1; row++) {
-                for (int col = 1; col < treeMap[row].length - 1; col++) {
-
-                    if (checkIfTreeIsVisibleInTop(treeMap, row, col) ||
-                            checkIfTreeIsVisibleInBottom(treeMap, row, col) ||
-                            checkIfTreeIsVisibleInLeft(treeMap, row, col) ||
-                            checkIfTreeIsVisibleInRight(treeMap, row, col)
-                    ) {
-                        countOfVisibleInteriorTrees += 1;
-                    }
-
-                }
-            }
-
-            System.out.println("Visible in edges: " + ((countOfRows * 2 + ((countOfRows - 2) + (countOfRows - 2))) + countOfVisibleInteriorTrees));
-
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+        if (FILE_NAME.endsWith("input.txt")) {
+            sizeTreeMap = 99;
         }
+
+        int countOfRows = 0;
+
+        int[][] treeMap = new int[sizeTreeMap][sizeTreeMap];
+
+        List<String> input = InputData.get(FILE_NAME);
+
+        for (String line : input) {
+
+            if (line.isEmpty()) {
+                continue;
+            }
+
+            treeMap[countOfRows] = Arrays.stream(line.split(""))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            countOfRows += 1;
+
+        }
+
+        int countOfVisibleInteriorTrees = 0;
+        for (int row = 1; row < treeMap.length - 1; row++) {
+            for (int col = 1; col < treeMap[row].length - 1; col++) {
+
+                if (checkIfTreeIsVisibleInTop(treeMap, row, col) ||
+                        checkIfTreeIsVisibleInBottom(treeMap, row, col) ||
+                        checkIfTreeIsVisibleInLeft(treeMap, row, col) ||
+                        checkIfTreeIsVisibleInRight(treeMap, row, col)
+                ) {
+                    countOfVisibleInteriorTrees += 1;
+                }
+
+            }
+        }
+
+        System.out.println("(Part 1). Visible in edges: " +
+                                   ((countOfRows * 2 + ((countOfRows - 2) + (countOfRows - 2))) + countOfVisibleInteriorTrees)
+        );
+
     }
 
     private static boolean checkIfTreeIsVisibleInTop(int[][] treeMap, int currentRow, int currentCol) {
--- src/Year2022/day8/Day8Part2.java
+++ src/Year2022/day8/Day8Part2.java
@@ -1,65 +1,63 @@
 package Year2022.day8;
 
 import shared.Constant;
+import shared.InputData;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
 public class Day8Part2 {
 
+    //    private static final String FILE_NAME = Constant.BASE_PATH_2022 + "day8\\sample.txt";
+    private static final String FILE_NAME = Constant.BASE_PATH_2022 + "day8\\input.txt";
+
     public static void main(String[] args) {
-        try {
-            File    inputFile = new File(Constant.BASE_PATH_2022 + "day8\\input.txt");
-            Scanner myReader  = new Scanner(inputFile);
+        int sizeTreeMap = 5;
 
-            int countOfRows = 0;
-
-            int[][] treeMap = new int[99][99];
-
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-
-                if (data.isEmpty()) {
-                    continue;
-                }
-
-                treeMap[countOfRows] = Arrays.stream(data.split(""))
-                        .mapToInt(Integer::parseInt)
-                        .toArray();
-                countOfRows += 1;
-
-            }
-            myReader.close();
-
-            List<Integer> scoreList = new ArrayList<>();
-
-            for (int row = 1; row < treeMap.length - 1; row++) {
-                for (int col = 1; col < treeMap[row].length - 1; col++) {
-
-                    scoreList.add(
-                            checkIfTreeIsVisibleInTop(treeMap, row, col) *
-                                    checkIfTreeIsVisibleInBottom(treeMap, row, col) *
-                                    checkIfTreeIsVisibleInLeft(treeMap, row, col) *
-                                    checkIfTreeIsVisibleInRight(treeMap, row, col)
-                    );
-
-                }
-            }
-
-            scoreList.sort(Collections.reverseOrder());
-
-            System.out.println(scoreList.get(0));
-
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+        if (FILE_NAME.endsWith("input.txt")) {
+            sizeTreeMap = 99;
         }
+
+        int countOfRows = 0;
+
+        int[][] treeMap = new int[sizeTreeMap][sizeTreeMap];
+
+        List<String> input = InputData.get(FILE_NAME);
+
+        for (String line : input) {
+
+            if (line.isEmpty()) {
+                continue;
+            }
+
+            treeMap[countOfRows] = Arrays.stream(line.split(""))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            countOfRows += 1;
+
+        }
+
+        List<Integer> scoreList = new ArrayList<>();
+
+        for (int row = 1; row < treeMap.length - 1; row++) {
+            for (int col = 1; col < treeMap[row].length - 1; col++) {
+
+                scoreList.add(
+                        checkIfTreeIsVisibleInTop(treeMap, row, col) *
+                                checkIfTreeIsVisibleInBottom(treeMap, row, col) *
+                                checkIfTreeIsVisibleInLeft(treeMap, row, col) *
+                                checkIfTreeIsVisibleInRight(treeMap, row, col)
+                );
+
+            }
+        }
+
+        scoreList.sort(Collections.reverseOrder());
+
+        System.out.println("(Part 2). " + scoreList.get(0));
+
     }
 
     private static int checkIfTreeIsVisibleInTop(int[][] treeMap, int currentRow, int currentCol) {
