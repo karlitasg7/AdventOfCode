e1e1b887eebd5f2c54645a00b6a92c69589cd68b
Karla Sequen <karlasequen@gmail.com>
2023-01-16 16:15:16 -0600

    advent 2015 day 4

---
+++ src/Year2015/Day4/Day4.java
@@ -0,0 +1,61 @@
+package Year2015.Day4;
+
+import shared.Constant;
+import shared.InputData;
+
+import java.util.List;
+
+public class Day4 {
+
+    //    private static final String FILE_NAME = Constant.BASE_PATH_2015 + "day4\\sample.txt";
+    private static final String FILE_NAME = Constant.BASE_PATH_2015 + "day4\\input.txt";
+
+    public static void main(String[] args) {
+
+        List<String> input = InputData.get(FILE_NAME);
+
+        for (String line : input) {
+            if (line.isEmpty()) {
+                continue;
+            }
+
+            System.out.println("===================");
+            System.out.println(line);
+            System.out.println("(Part 1). " + calculateMD5(line, "00000"));
+            System.out.println("(Part 2). " + calculateMD5(line, "000000"));
+        }
+
+    }
+
+    private static int calculateMD5(String secretKey, String startWith) {
+
+        int i = 1;
+
+        while (true) {
+            String newSecretKey = secretKey + i;
+            String hash         = getMD5(newSecretKey);
+
+            if (hash.startsWith(startWith)) {
+                System.out.println("Hash is = " + hash);
+                return i;
+            }
+            i++;
+        }
+
+    }
+
+    private static String getMD5(String secretKey) {
+        try {
+            java.security.MessageDigest md    = java.security.MessageDigest.getInstance("MD5");
+            byte[]                      array = md.digest(secretKey.getBytes());
+            StringBuffer                sb    = new StringBuffer();
+            for (int i = 0; i < array.length; ++i) {
+                sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
+            }
+            return sb.toString();
+        } catch (java.security.NoSuchAlgorithmException e) {
+        }
+        return "";
+    }
+
+}
