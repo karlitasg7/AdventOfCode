509a5db85c5e442201bdb31a0a30a110fc24ec3f
Karla Sequen <karlasequen@gmail.com>
2023-02-01 20:57:06 -0600

    2015 day 12

--- src/main/java/com/karlasequen/Year2015/Day12/Day12.java
+++ src/main/java/com/karlasequen/Year2015/Day12/Day12.java
@@ -1,8 +1,13 @@
 package com.karlasequen.Year2015.Day12;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.karlasequen.shared.Constant;
 import com.karlasequen.shared.InputData;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -25,8 +30,51 @@ public class Day12 {
             }
 
             System.out.println("(Part 1). " + total);
-        }
 
+        }
+        part2();
+
+    }
+
+    private static void part2() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode root = mapper.readTree(new File(FILE_NAME));
+            System.out.println("(Part 2). " + addIntegersInJson(root, 0));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int addIntegersInJson(JsonNode node, int sum) {
+        int result = sum;
+        if (node.isInt())
+            result += sum + node.asInt();
+        if (node.isArray()) {
+            for (JsonNode n : node) {
+                result += addIntegersInJson(n, sum);
+            }
+        }
+        if (node.isObject() && !objectHasRedAsValue(node)) {
+            Iterator<JsonNode> it = node.elements();
+            while (it.hasNext()) {
+                JsonNode elem = it.next();
+                result += addIntegersInJson(elem, sum);
+            }
+        }
+        return result;
+    }
+
+    private static boolean objectHasRedAsValue(JsonNode node) {
+        boolean            result = false;
+        Iterator<JsonNode> it     = node.iterator();
+        while (it.hasNext()) {
+            if (it.next().asText().equals("red")) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 
 }
