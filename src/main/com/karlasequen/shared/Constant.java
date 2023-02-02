9dd703559556fe4dcb7e21a4717fefa9b2b84ae1
Karla Sequen <karlasequen@gmail.com>
2023-02-02 09:03:37 -0600

    reorder packages and classes

---
+++ src/main/com/karlasequen/shared/Constant.java
@@ -0,0 +1,9 @@
+package com.karlasequen.shared;
+
+public class Constant {
+
+    public static final String BASE_PATH_2015 = "src/main/com/karlasequen/Year2015/";
+    public static final String BASE_PATH_2021 = "src/main/com/karlasequen/Year2021/";
+    public static final String BASE_PATH_2022 = "src/main/com/karlasequen/Year2022/";
+
+}
--- src/main/java/com/karlasequen/shared/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.karlasequen.shared;
-
-public class Constant {
-
-    public static final String BASE_PATH_2015 = "src/main/java/com/karlasequen/Year2015/";
-    public static final String BASE_PATH_2021 = "src/main/java/com/karlasequen/Year2021/";
-    public static final String BASE_PATH_2022 = "src/main/java/com/karlasequen/Year2022/";
-
-}
