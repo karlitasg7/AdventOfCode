5803bfeba9855d4c55e65df5928045842f2176c8
Karla Sequen <karlasequen@gmail.com>
2024-11-13 22:04:56 -0600

    move adventjs and halloween to different repo

--- js/Year2023/Day2.js
+++
@@ -1,9 +0,0 @@
-function manufacture(gifts, materials) {
-    return gifts.filter(gift => {
-        return gift.split('').every(c => materials.includes(c))
-    })
-}
-
-console.log(manufacture(['train', 'bear', 'ball'], 'tronesa')) // ["", ""]
-console.log(manufacture(['game', 'puzzle'], 'jlepuz')) // ["puzzle"]
-console.log(manufacture(['book', 'ps5'], 'psli')) // []
